079d3aa7a491daec1c30568b3dccbe104fb7e562
scheraghpourahmadmah <scheraghpourahmadmah@myseneca.ca>
2023-02-02 17:58:17 -0500

    first commit

---
+++ Api/Services/ChatService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Api.Services
+{
+    public class ChatService
+    {
+        // Key , Value eg: { {"john", "asdsah!@12321"}, {"david", "ajsdhsaj2312"}}
+        private static readonly Dictionary<string, string> Users = new Dictionary<string, string>();
+
+        public bool AddUserToList(string userToAdd)
+        {
+            lock(Users)
+            {
+                foreach(var user in Users)
+                {
+                    if(user.Key.ToLower() == userToAdd.ToLower())
+                    {
+                        return false;
+                    }
+                }
+                Users.Add(userToAdd, null);
+                return true;
+            }
+
+        }
+    }
+}
